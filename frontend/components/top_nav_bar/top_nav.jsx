78ea5425c223b7f861a62ae253ea9a5ae2806f0c
Novirius <professional.nt7@gmail.com>
2020-01-08 18:27:22 -0800

    navbar

--- frontend/components/top_nav_bar/top_nav.jsx
+++ frontend/components/top_nav_bar/top_nav.jsx
@@ -3,15 +3,47 @@ import {Link} from 'react-router-dom';
 
 const TopNav = (props) => {
     const loggedOut = () => (
-        <div className="topnav">
-            <div>
-                <Link to="/">Home Page</Link>
+        <div className="top-nav">
+            <div className="top-nav-container">
+                <div className="top-nav-logo-wrapper">
+                    <div className="top-nav-logo">
+                    <button>FLIP.me</button>
+                    </div>
+                </div>
+                <div className="top-nav-transition-wrapper">
+                    <div className="top-nav-transition">
+                        <div className="top-nav-links-container">
+                            <div className="top-nav-search-wrapper">
+                                <div className="top-nav-search">
+                                    <span>Search</span>
+                                </div>
+                            </div>
+                            <div className="top-nav-browse-wrapper">
+                                <div className="top-nav-browse">
+                                    <span>Browse</span>
+                                </div>
+                            </div>
+                            <div className="top-nav-create-wrapper">
+                                <div className="top-nav-create">
+                                    <span>Create</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="top-nav-user-container">
+                            <div className="top-nav-signup-wrapper">
+                                <div className="top-nav-signup">
+                                    <span>Sign Up</span>
+                                </div>
+                            </div>
+                            <div className="top-nav-login-wrapper">
+                                <div className="top-nav-login">
+                                    <span>Log In</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <button type="button">Search</button>
-            <button type="button">Browse</button>
-            <button type="button">Create</button>
-            <button type="button" onClick={() => props.openModal('signup')}>Sign Up</button>
-            <button type="button" onClick={() => props.openModal('login')}>Log In</button>
         </div>
     )
     const loggedIn = () => (
